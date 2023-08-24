7dc23f2147072e3eff1d997c24f99a345ccf9a00
Elham Qafouri <elham.qafouri.91@gmail.com>
2023-08-24 14:22:36 +0330

    feat: add navbar

---
+++ views/layout/main.php
@@ -0,0 +1,64 @@
+<?php
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">BOLBOL</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link {home}" aria-current="page" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link  {pricing}" aria-current="page" href="pricing">pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link  {album}" aria-current="page" href="album">album</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {blog}" aria-current="page" href="blog">blog</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {checkout}" aria-current="page" href="checkout">checkout</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {contact}" aria-current="page" href="contact">contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {features}" aria-current="page" href="features">features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {FQA}" aria-current="page" href="FAQ">FAQ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {product}" aria-current="page" href="product">product</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link {signup}" aria-current="page" href="signup">signup</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {login}" aria-current="page" href="login">login</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+</body>
+</html>
