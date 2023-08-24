b8190345dde8c6b98d3ba9bc082497906cf370d3
Elham Qafouri <elham.qafouri.91@gmail.com>
2023-08-24 15:18:57 +0330

    feat: add database

--- public/index.php
+++ public/index.php
@@ -1,1 +1,24 @@
 <?php
+require_once __DIR__ . "/../vendor/autoload.php";
+
+USE app\core\Application;
+USE app\Controllers\SiteController;
+USE app\Controllers\AuthController;
+
+$app = new Application(dirname(__DIR__));
+
+$app->getRouter()->get('/',[SiteController::class, 'home']);
+$app->getRouter()->get('/features',[SiteController::class, 'features']);
+$app->getRouter()->get('/blog',[SiteController::class, 'blog']);
+$app->getRouter()->get('/_404',[SiteController::class,'_404']);
+//$app->getRouter()->get('/_404','_404');
+
+$app->getRouter()->get('/login',[AuthController::class,'login']);
+$app->getRouter()->post('/login',[AuthController::class,'login']);
+$app->getRouter()->get('/signup',[AuthController::class,'register']);
+$app->getRouter()->post('/signup',[AuthController::class,'register']);
+
+
+$app->run();
+
+
